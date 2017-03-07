c59e21311da2c65b0d9821bbcf3e6ea3b063f8f7
sunht <beyondsht@126.com>
2017-03-07 12:47:05 +0800

    add camera params to func launch.

--- www/camctrl.js
+++ www/camctrl.js
@@ -10,10 +10,10 @@ module.exports = {
         },"CamCtrl","echo", [str]);
     },
 
-    launch:function(str, callback){
-        console.log("launch:" + str);
+    launch:function(camIp, camPort, uName, pwd, tunel, callback){
+        console.log("launch camera activity:" + camIp);
         cordova.exec(callback, function(err){
             callback("something wrong.");
-        }, "CamCtrl","launch", [str]);
+        }, "CamCtrl","launch", [camIp, camPort, uName, pwd, tunel]);
     }
 };
