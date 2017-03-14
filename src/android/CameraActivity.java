ebca86308959ce64a3b93b0233dfe8cc7cbecb46
sunht <beyondsht@126.com>
2017-03-14 18:04:43 +0800

    add import toast.

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -22,6 +22,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.hikvision.netsdk.ExceptionCallBack;
 import com.hikvision.netsdk.HCNetSDK;
